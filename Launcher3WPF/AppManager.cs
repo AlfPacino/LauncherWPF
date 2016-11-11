253118feb668ec9e2f06c7b6f19f7237d1b683b6
Alexey Kovalchuk <locopwnz@yandex.ua>
2016-11-11 23:53:09 +0200

    development start

---
+++ Launcher3WPF/AppManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Launcher3WPF
+{
+    public class AppManager
+    {
+        public AppManager()
+        {
+            this.appCount = 0;
+            apps = new List<AppToLaunch>();
+        }
+        public AppManager(int appCount)
+        {
+            this.appCount = appCount;
+        }
+
+        public int appCount;
+        public List<AppToLaunch> apps;
+    }
+
+    public struct AppToLaunch
+    {
+        public AppToLaunch(string exePath, BitmapImage bmpIcon)
+        {
+            this.exePath = exePath;
+            this.bmpIcon = bmpIcon;
+        }
+        public string exePath;
+        public BitmapImage bmpIcon;
+    }
+}
---
+++ Launcher3WPF/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Launcher3WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        AppManager appManager;
+
+        private void Window_Loaded(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists("Config.xml"))
+                {
+                    // deserealisation
+                    using (Stream stream = new FileStream("Config.xml", FileMode.Open))
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(AppManager));
+                        appManager = (AppManager)serializer.Deserialize(stream);
+                    }
+                }
+                else
+                {
+                    appManager = new AppManager(0);
+                }
+            }
+            catch (Exception exep)
+            {
+                appManager = new AppManager(0);
+                System.Windows.MessageBox.Show("Error while decoding Config.xml: " + Environment.NewLine + exep.Message);
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if(openFileDialog.FileName.EndsWith(".exe"))
+                {
+                    // add new app
+                    BitmapImage bmp = new BitmapImage(new Uri("C:\\Users\\Dex\\Desktop\\кот.jpg"));
+
+
+                    // serealisation data to xml
+                    using (Stream writer = new FileStream("Config.xml", FileMode.Create))
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(AppManager));
+                        serializer.Serialize(writer, appManager);
+                    }
+                }
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            // delete app 
+
+            // serealisation to xml
+            using (Stream writer = new FileStream("Config.xml", FileMode.Create))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(AppManager));
+                serializer.Serialize(writer, appManager);
+            }
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        { 
+            Environment.Exit(0);
+        }
+
+    }
+}
