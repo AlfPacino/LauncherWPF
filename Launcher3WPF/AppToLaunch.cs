e141b67e6c76ad8168225fc11beba83087b54fed
Alexey K <locopwnz@yandex.ua>
2018-02-01 23:09:55 +0200

    refactoring

--- Launcher3WPF/AppToLaunch.cs
+++ Launcher3WPF/AppToLaunch.cs
@@ -1,43 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows.Media.Imaging;
 using System.Xml.Serialization;
-using System.IO;
-using System.Windows.Media;
 
 namespace Launcher3WPF
 {
-    /*
-    public class AppManager
-    {
-        public AppManager()
-        {
-            this.appCount = 0;
-            apps = new List<AppToLaunch>();
-        }
-        public AppManager(int appCount)
-        {
-            this.appCount = appCount;
-        }
-
-        public int appCount;
-        public List<AppToLaunch> apps;
-        public void AddApp(string pathToExe, BitmapImage bmpIcon)
-        {
-            apps.Add(new AppToLaunch(pathToExe, bmpIcon));
-            appCount++;
-        }
-    }
-    */
     public struct AppToLaunch
     {
         public AppToLaunch(string exePath, BitmapSource bmpImage)
         {
             this.exePath = exePath;
-            //this.bmpImage = bmpImage;
 
             // convert BitmapImage => byte array
             this.bmpBytes = null;
@@ -70,10 +41,7 @@ namespace Launcher3WPF
             image.Freeze();
             return image;
         }
-        /*
-        [XmlIgnore]
-        public BitmapSource bmpImage;
-        */
+
         [XmlElement("Image")]
         public byte[] bmpBytes;
 
--- Launcher3WPF/MainWindow.xaml.cs
+++ Launcher3WPF/MainWindow.xaml.cs
@@ -1,26 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml.Serialization;
-using System.IO;
-using System.Diagnostics;
 
 namespace Launcher3WPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -219,6 +207,5 @@ namespace Launcher3WPF
         {
             Environment.Exit(0);
         }
-
     }
 }
